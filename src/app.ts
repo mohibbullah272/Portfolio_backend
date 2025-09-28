63eb562df2d974afa65a2751225e99aaafcd2bcf
mohibbullah272 <bullahmohi50@gmail.com>
2025-09-28 16:18:18 +0600

    add intial setup of this server

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import compression from "compression";
+import cors from "cors";
+import express from "express";
+
+
+const app = express();
+
+// Middleware
+app.use(cors()); 
+app.use(compression()); 
+app.use(express.json()); 
+
+app.use(cors());
+
+
+
+
+app.get("/", (_req, res) => {
+  res.send("API is running");
+});
+
+
+// 404 Handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+
+export default app;
